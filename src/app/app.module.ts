7ec48a9cdc8b3afaada29a63c2fc7138d61043c2
Ronin <testronintest@gmail.com>
2023-11-19 23:17:15 +0300

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ReadinglistComponent } from './app/readinglist/readinglist.component';
+import {AppComponent} from './app.component';
+import {ReadinglistComponent} from './app/readinglist/readinglist.component';
+import {FormsModule} from "@angular/forms";
+import {LibraryModel} from "./app/models/librarymodel";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReadinglistComponent
+    ReadinglistComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDividerModule
   ],
-  providers: [],
+  providers: [LibraryModel],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app/readinglist/readinglist.component.ts
+++ src/app/app/readinglist/readinglist.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {NgModel} from "@angular/forms";
-import {LibraryModel} from "../models/librarymodel";
+import {LibraryModel, BookItems} from "../models/librarymodel";
 
 
 @Component({
@@ -9,13 +9,28 @@ import {LibraryModel} from "../models/librarymodel";
   styleUrls: ['./readinglist.component.css']
 })
 export class ReadinglistComponent {
-  model = new LibraryModel();
 
-  getName(){
-    return this.model.library;
+  constructor(private libraryModel: LibraryModel) {
   }
 
-  getBookItems(){
-    return this.model.bookItems;
+  getName() {
+    return this.libraryModel.library;
   }
+
+  getReadingBookItems() {
+    return this.libraryModel.bookItems
+      .filter(item => !item.action);
+  }
+
+  getReadedBookItems(){
+    return this.libraryModel.bookItems
+      .filter(item => item.action);
+  }
+
+  addItem(value:string){
+    if (value.trim() !== ''){
+      this.libraryModel.bookItems.push(new BookItems(value,false));
+    }
+  }
+
 }
